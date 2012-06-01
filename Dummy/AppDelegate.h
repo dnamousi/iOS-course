b39bcf3f538287f13a9700516e29affa26e070e6
Dastan Namousi <dastan@localhost.(none)>
2012-06-01 11:56:09 +0200

    Initial Commit

---
+++ Dummy/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Dummy
+//
+//  Created by Dastan Namousi on 2012-06-01.
+//  Copyright (c) 2012 Chalmers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Dummy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dummy
+//
+//  Created by Dastan Namousi on 2012-06-01.
+//  Copyright (c) 2012 Chalmers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
