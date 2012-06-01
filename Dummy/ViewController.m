2ca9b15f92a7bbf3789af82107823ae3072931f6
Dastan Namousi <dastan@localhost.(none)>
2012-06-01 12:01:17 +0200

    Added another comment.

--- Dummy/ViewController.m
+++ Dummy/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     // I be adding the code.
+    // Adding another comment. Crayzay...
 }
 
 - (void)viewDidUnload
